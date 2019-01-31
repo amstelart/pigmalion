17af488dfa3a4e85b20078d1c5ae42d2920d00ef
amstelart <amsteldroid@gmail.com>
2019-02-01 00:00:30 +0300

    Update 2019-01-31T21:00:22.121Z

--- js/script.js
+++ js/script.js
@@ -300,23 +300,15 @@ $( document ).ready(function() {
     }
   });
 
-  // var mainSlider = $('#home-intro-carousel');
-  // mainSlider.owlCarousel({
-  //   items: 1,
-  //   nav: true,
-  //   navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 3px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 3px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-  //   margin: 0,
-  //   loop: false,
-  //   dots: false,
-  //   center: false,
-  //   onInitialized: function(e) {
-  //     $('.counter-out').text('1 / ' + this.items().length)
-  //     console.log();
-  //   }
-  // });
-  // mainSlider.on('changed.owl.carousel', function(e) {
-  //   $('.counter-out').text(++e.page.index  + ' / ' + e.page.count)
-  // });
+  $('.custom-nav .get-search').on('click', function(e) {
+		$('.page-header__search').addClass("active");
+		e.preventDefault();
+	});
+
+  $('.search-close').on('click', function(e) {
+		$('.page-header__search').removeClass("active");
+		e.preventDefault();
+	});
 
   $('#home-intro-carousel').on('initialized.owl.carousel changed.owl.carousel', function(e) {
     if (!e.namespace) {
